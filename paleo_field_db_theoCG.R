a577538818f8108531777e3c4745fa496668bfbf
ediscamps <78740956+ediscamps@users.noreply.github.com>
2024-07-25 11:47:38 +0200

    Update paleo_field_db_theoCG.R

--- paleo_field_db_theoCG.R
+++ paleo_field_db_theoCG.R
@@ -2,6 +2,7 @@
 ### paleo_field_db_theoCG : merging field data to create the general spatial database, adapted to Combe Grenal excavations
 # https://github.com/ediscamps/paleo_field_db 
 # E. Discamps emmanuel.discamps@cnrs.fr
+# last edit: 25th July 2024
 
 library("this.path")
 setwd(here())
@@ -13,7 +14,7 @@ baseWD <- getwd()
 # importing new data from CSVs
 setwd("./csv_to_import")
 ### SELECT THE PROPER FILE EXTENSION .csv or .txt
-listCSV <- list.files(pattern= ".txt", full.names = "F")
+listCSV <- list.files(pattern= ".csv", full.names = "F")
 
 extractDATA <- function(x){
   filenameCSV <- x
@@ -26,34 +27,41 @@ extractDATA <- function(x){
 newdataLIST <- lapply(listCSV, extractDATA)
 setwd(baseWD)
 
-
 #creating dataframe
 require(dplyr)
 
 dataTOT <- bind_rows(newdataLIST)
 colnames(dataTOT) <- c("Filename", "Point","X","Y","Z","Code")
 
-#removing toponulle and REPERES
+#extracting TOPO (topographic points taken in the field that should be kept) 
 dataTOPO <- filter(dataTOT, Code == "TOPO")
+dataTOPO <- filter(dataTOT, Code == "topo")
+
+#extracting COIN (altitudes on the corners of a square taken at the end of a décapage)
 dataCOIN <- filter(dataTOT, grepl("-", dataTOT$Point))
 
+#creating dataOK (only archaeological objects and center of buckets)
+#removing toponulle (topographic points taken in the field that should not be kept), REPERES, TOPO and COIN
 dataOK <- subset(dataTOT, Code != "toponulle")
-dataOK <- subset(dataTOT, Code != "TOPONULLE")
+dataOK <- subset(dataOK, Code != "toponulle ")
+dataOK <- subset(dataOK, Code != "TOPONULLE")
+dataOK <- subset(dataOK, Code != "toonulle")
 dataOK <- subset(dataOK, Code != "REPERES")
 dataOK <- subset(dataOK, Point != "CTRL6")
 dataOK <- subset(dataOK, Point != "CTRL7")
+dataOK <- subset(dataOK, Point != "CTRL8")
 dataOK <- subset(dataOK, Point != "st")
 dataOK <- subset(dataOK, Point != "st2")
 dataOK <- subset(dataOK, Point != "st3")
 dataOK <- subset(dataOK, Code != "TOPO")
+dataOK <- subset(dataOK, Code != "topo")
 dataOK <- subset(dataOK, !grepl("-", dataOK$Point))
 
-
-#removing .csv or .txt extension of Filename
+#extracting the filename in a new column (Filename), while removing .csv or .txt extension of Filename
 ### SELECT THE PROPER FILE EXTENSION .csv or .txt
-dataOK$Filename <- strsplit(dataOK$Filename, ".txt")
-dataTOPO$Filename <- strsplit(dataTOPO$Filename, ".txt")
-dataCOIN$Filename <- strsplit(dataCOIN$Filename, ".txt")
+dataOK$Filename <- strsplit(dataOK$Filename, ".csv")
+dataTOPO$Filename <- strsplit(dataTOPO$Filename, ".csv")
+dataCOIN$Filename <- strsplit(dataCOIN$Filename, ".csv")
 
 #splitting the code
 codeTMP <- as.data.frame(do.call(rbind,strsplit(dataOK$Code, "_")))
@@ -98,11 +106,6 @@ dataOK$Trench = case_when(
   (dataOK$CarreTheo == "J51B") ~ "51ouest",
   (dataOK$CarreTheo == "J51D") ~ "51ouest")
 
-dataOK$Yminus <- -dataOK$Y
-dataOK$Annee <- "2023"
-
-
-
 #CALCULATING TRUE SQUARES
 part1 <- case_when(
   (dataOK$Y >= 104 & dataOK$Y < 105) ~ "F",
@@ -114,11 +117,11 @@ part1 <- case_when(
   (dataOK$Y >= 105 | dataOK$Y < 99) ~ "NA")
 
 part2 <- case_when(
-    (dataOK$X >= 52 & dataOK$X < 53) ~ "52",
-    (dataOK$X >= 51 & dataOK$X < 52) ~ "51",
-    (dataOK$X >= 50 & dataOK$X < 51) ~ "50",
-    (dataOK$X >= 49 & dataOK$X < 50) ~ "49",
-    (dataOK$X >= 53 | dataOK$X < 49) ~ "NA")
+  (dataOK$X >= 52 & dataOK$X < 53) ~ "52",
+  (dataOK$X >= 51 & dataOK$X < 52) ~ "51",
+  (dataOK$X >= 50 & dataOK$X < 51) ~ "50",
+  (dataOK$X >= 49 & dataOK$X < 50) ~ "49",
+  (dataOK$X >= 53 | dataOK$X < 49) ~ "NA")
 
 subX <- dataOK$X - floor(dataOK$X)
 subY <- dataOK$Y - floor(dataOK$Y)
@@ -140,6 +143,9 @@ rm(subY)
 table(dataOK$CarreVrai == dataOK$CarreTheo)
 
 
+#creating other columns in dataOK
+dataOK$Yminus <- -dataOK$Y
+dataOK$Annee <- "2024"
 dataOK$offsetcorr <- ""
 dataOK$posapprox <- ""
 dataOK$FabID <- ""
@@ -148,20 +154,21 @@ dataOK$Pendage <- ""
 dataOK$Notes <- ""
 dataOK$UA <- ""
 
+#creating a "notes" column in dataTOPO
 dataTOPO$Notes <- ""
 
 #reorganizing column order and names
 dataOK <- cbind(dataOK$Annee,dataOK[,1:4],dataOK$Yminus, dataOK$Z,dataOK$Code,
-                 dataOK$CarreVrai, dataOK$CarreTheo, dataOK$Dec,
-                 dataOK$US, dataOK$UA,
-                 dataOK$offsetcorr, dataOK$posapprox, dataOK$Notes,
-                 dataOK$FabID, dataOK$Orient, dataOK$Pendage,
-                 dataOK$Trench)
+                dataOK$CarreVrai, dataOK$CarreTheo, dataOK$Dec,
+                dataOK$US, dataOK$UA,
+                dataOK$offsetcorr, dataOK$posapprox, dataOK$Notes,
+                dataOK$FabID, dataOK$Orient, dataOK$Pendage,
+                dataOK$Trench)
 
 colnames(dataOK) <- c("Year","Filename","Point","X","Y","Yminus","Z","Code",
-"CarreVrai", "CarreTheo", "Dec",
-"USfield", "UA","offsetcorr", "posapprox", "Notes",
-"FabID", "Orient", "Pendage","Trench")
+                      "CarreVrai", "CarreTheo", "Dec",
+                      "USfield", "UA","offsetcorr", "posapprox", "Notes",
+                      "FabID", "Orient", "Pendage","Trench")
 
 
 #moving CSVs
@@ -172,30 +179,43 @@ moveCSV <- function(x){
 
 lapply(listCSV, moveCSV)
 
+
 #import data previously imported
-if(file.exists("CG23_THEO.xlsx")){
+#be sure to change the filename with the appropriate Excel file (full database)
+#ATTENTION: in the process, some sheets of that Excel file will be deleted (the script returns which ones)
+if(file.exists("CG24_THEO.xlsx")){
   library(readxl)
-  previous_dataOK <- read_excel("CG23_THEO.xlsx", sheet = "dataOK", 
+  previous_dataOK <- read_excel("CG24_THEO.xlsx", sheet = "dataOK", 
                                 col_types = c(rep("text",3),
                                               rep("numeric",4), 
                                               rep("text",10),
                                               rep("numeric",2),
                                               "text"))
-  previous_dataTOPO <- read_excel("CG23_THEO.xlsx", sheet = "dataTOPO")
-  previous_dataCOIN <- read_excel("CG23_THEO.xlsx", sheet = "dataCOIN")
+  previous_dataTOPO <- read_excel("CG24_THEO.xlsx", sheet = "dataTOPO")
+  previous_dataCOIN <- read_excel("CG24_THEO.xlsx", sheet = "dataCOIN")
+  list_sheets <- excel_sheets("CG24_THEO.xlsx")
+  list_sheets <- list_sheets[list_sheets != "dataOK"]
+  list_sheets <- list_sheets[list_sheets != "dataTOPO"]
+  list_sheets <- list_sheets[list_sheets != "dataCOIN"]
+  list_sheets <- list_sheets[list_sheets != "notes"]
+  notes <- read_excel("CG24_THEO.xlsx", sheet = "notes")
+  print("ATTENTION: the following sheets won't be imported:")
+  print(list_sheets)
   dataOK <- rbind(previous_dataOK, dataOK)
   dataTOPO <- rbind(previous_dataTOPO, dataTOPO)
   dataCOIN <- rbind(previous_dataCOIN, dataCOIN)
 }
 
+
 #writing Excel file
 require(WriteXLS)
 setwd(baseWD)
-WriteXLS(c("dataOK","dataTOPO","dataCOIN"),"CG23_THEO.xlsx")
+WriteXLS(c("dataOK","dataTOPO","dataCOIN","notes"),"CG24_THEO.xlsx")
 
 #exporting bucket points
 require(WriteXLS)
-WriteXLS(filter(dataOK, Code =="Seau"),"CG23_seaux.xlsx")
+WriteXLS(filter(dataOK, Code =="Seau"),"CG24_seaux.xlsx")
+
 
 ##RUNNING SEAHORS for checking
 require("SEAHORS")
